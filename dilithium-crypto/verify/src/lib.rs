d7d17ea713b2c5a8694ffbadbbf97fa5b9448a0a
NIK <nheger@gmail.com>
2025-02-23 18:49:14 +0800

    implementation of verify

--- dilithium-crypto/verify/src/lib.rs
+++ dilithium-crypto/verify/src/lib.rs
@@ -1,9 +1,26 @@
 #![no_std]
 
-use rusty_crystals_dilithium::ml_dsa_87;
+use rusty_crystals_dilithium::ml_dsa_87::PublicKey;
 
+// pub fn from_bytes(bytes: &[u8]) -> PublicKey {
+// pub fn verify(&self, msg: &[u8], sig: &[u8], ctx: Option<&[u8]>) -> bool {
 
-pub fn verify(left: u64, right: u64) -> u64 {
-    left + right
+pub fn verify(pub_key: &[u8], msg: &[u8], sig: &[u8]) -> bool {
+    let pk = PublicKey::from_bytes(pub_key);
+    pk.verify(msg, sig, None)
 }
 
+
+
+// Do the same except with the message, not the signature...
+
+// let sig = ml_dsa_87::sign(&msg, &sk, None);
+// let mut combined = Vec::new();
+// combined.extend_from_slice(&pk.to_bytes());
+// combined.extend_from_slice(&sig);
+// let extrinsic = UncheckedExtrinsic::new_signed(
+//     call,
+//     Address::Id(account_id),
+//     RezMultiSignature::Rez(combined),
+//     signed_extra,
+// );
