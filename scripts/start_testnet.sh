3d66288e6e1a18675edabf89ab050654dfe43275
NIK <nheger@gmail.com>
2025-05-28 12:41:55 +0800

    start script

--- scripts/start_testnet.sh
+++ scripts/start_testnet.sh
@@ -8,4 +8,4 @@ rm -rf /tmp/validator1
   --name ResonanceLiveTestnetNode \
   --experimental-rpc-endpoint "listen-addr=127.0.0.1:9944,methods=unsafe,cors=all" \
   --node-key cffac33ca656d18f3ae94393d01fe03d6f9e8bf04106870f489acc028b214b15 \
-#   --validator
+  --validator
