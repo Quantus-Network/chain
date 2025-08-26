ce65edb9a67605d3a1fb099a550a2d601461305b
Nikolaus Heger <nheger@gmail.com>
2025-08-26 20:42:31 +0800

    Update client/network/sync/src/engine.rs
    Co-authored-by: Dastan <88332432+dastansam@users.noreply.github.com>

--- client/network/sync/src/engine.rs
+++ client/network/sync/src/engine.rs
@@ -1008,7 +1008,7 @@ where
 				let is_major_syncing = self.is_major_syncing.load(Ordering::Relaxed);
 				let peer_drop_threshold = self.strategy.peer_drop_threshold();
 				let peer_failures = self.peer_failures.entry(peer_id).or_insert(0);
-				*peer_failures = peer_failures.saturating_add(1);
+				peer_failures.saturating_inc();
 
 				let should_drop_peer = !is_major_syncing || *peer_failures >= peer_drop_threshold;
 				
