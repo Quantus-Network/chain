05e594d625aa5cea6bda5ab9ee7bcb129204fc9e
illuzen <illuzen@users.noreply.github.com>
2025-09-08 17:28:37 +0800

    remove unused

--- client/consensus/qpow/src/lib.rs
+++ client/consensus/qpow/src/lib.rs
@@ -10,7 +10,7 @@ use sp_api::{ProvideRuntimeApi, __private::BlockT};
 use sp_consensus_pow::Seal as RawSeal;
 use sp_consensus_qpow::QPoWApi;
 use sp_runtime::generic::BlockId;
-use std::{marker::PhantomData, sync::Arc, time::Duration};
+use std::{sync::Arc, time::Duration};
 
 pub use crate::worker::{MiningBuild, MiningHandle, MiningMetadata};
 
@@ -26,7 +26,7 @@ use sc_consensus::{
 use sp_block_builder::BlockBuilder as BlockBuilderApi;
 use sp_blockchain::HeaderBackend;
 use sp_consensus::{Environment, Error as ConsensusError, Proposer, SelectChain, SyncOracle};
-use sp_consensus_pow::{Seal, POW_ENGINE_ID};
+use sp_consensus_pow::POW_ENGINE_ID;
 use sp_inherents::{CreateInherentDataProviders, InherentDataProvider};
 use sp_runtime::{
 	generic::{Digest, DigestItem},
@@ -215,14 +215,6 @@ where
 		&self,
 		mut block: BlockImportParams<B>,
 	) -> Result<ImportResult, Self::Error> {
-		let best_header = self
-			.select_chain
-			.best_chain()
-			.await
-			.map_err(|e| format!("Fetch best chain failed via select chain: {}", e))
-			.map_err(ConsensusError::ChainLookup)?;
-		let best_hash = best_header.hash();
-
 		let parent_hash = *block.header.parent_hash();
 
 		if let Some(inner_body) = block.body.take() {
@@ -244,12 +236,8 @@ where
 
 		let inner_seal = fetch_seal::<B>(block.post_digests.last(), block.header.hash())?;
 
-		// Fetch difficulty directly using the client
-		let difficulty = qpow_get_difficulty::<B, C>(&*self.client, parent_hash)?;
-
 		let pre_hash = block.header.hash();
-		let pre_digest = find_pre_digest::<B>(&block.header)?;
-		let (verified, difficulty) = qpow_verify::<B, C>(
+		let verified = qpow_verify::<B, C>(
 			&*self.client,
 			&BlockId::hash(parent_hash),
 			&pre_hash,
@@ -270,69 +258,48 @@ where
 	}
 }
 
-/// A verifier for PoW blocks.
-pub struct PowVerifier<B: BlockT<Hash = H256>, C: ProvideRuntimeApi<B>> {
-	_marker: PhantomData<(B, C)>,
-}
-
-impl<B: BlockT<Hash = H256>, C> PowVerifier<B, C>
-where
-	C: ProvideRuntimeApi<B> + BlockBackend<B> + Send + Sync + 'static,
-	C::Api: QPoWApi<B>,
-{
-	pub fn new() -> Self {
-		Self { _marker: PhantomData }
-	}
-
-	fn check_header(&self, mut header: B::Header) -> Result<(B::Header, DigestItem), Error<B>> {
-		let hash = header.hash();
-
-		let (seal, inner_seal) = match header.digest_mut().pop() {
-			Some(DigestItem::Seal(id, seal)) => {
-				if id == POW_ENGINE_ID {
-					(DigestItem::Seal(id, seal.clone()), seal)
-				} else {
-					return Err(Error::WrongEngine(id));
-				}
-			},
-			_ => return Err(Error::HeaderUnsealed(hash)),
-		};
-
-		let pre_hash = header.hash();
-
-		// preliminary_verify is not used in QPoW (always None)
-		if !Option::<bool>::None.unwrap_or(true) {
-			return Err(Error::FailedPreliminaryVerify);
-		}
-
-		Ok((header, seal))
-	}
-}
-
-#[async_trait::async_trait]
-impl<B, C> Verifier<B> for PowVerifier<B, C>
+/// Extract the PoW seal from header into post_digests for later verification.
+async fn extract_pow_seal<B>(mut block: BlockImportParams<B>) -> Result<BlockImportParams<B>, String>
 where
 	B: BlockT<Hash = H256>,
-	C: ProvideRuntimeApi<B> + BlockBackend<B> + Send + Sync + 'static,
-	C::Api: QPoWApi<B>,
 {
-	async fn verify(
-		&self,
-		mut block: BlockImportParams<B>,
-	) -> Result<BlockImportParams<B>, String> {
-		let hash = block.header.hash();
-		let (checked_header, seal) = self.check_header(block.header)?;
+	let hash = block.header.hash();
+	let header = &mut block.header;
+	let block_hash = hash;
+	let seal_item = match header.digest_mut().pop() {
+		Some(DigestItem::Seal(id, seal)) => {
+			if id == POW_ENGINE_ID {
+				DigestItem::Seal(id, seal)
+			} else {
+				return Err(Error::<B>::WrongEngine(id).into());
+			}
+		}
+		_ => return Err(Error::<B>::HeaderUnsealed(block_hash).into()),
+	};
 
-		block.header = checked_header;
-		block.post_digests.push(seal);
-		block.post_hash = Some(hash);
-
-		Ok(block)
-	}
+	block.post_digests.push(seal_item);
+	block.post_hash = Some(hash);
+	Ok(block)
 }
 
 /// The PoW import queue type.
 pub type PowImportQueue<B> = BasicQueue<B>;
+
+/// Minimal verifier that extracts the PoW seal from header to post_digests.
+struct SimplePowVerifier;
+
+#[async_trait::async_trait]
+impl<B> Verifier<B> for SimplePowVerifier
+where
+	B: BlockT<Hash = H256>,
+{
+	async fn verify(
+		&self,
+		block: BlockImportParams<B>,
+	) -> Result<BlockImportParams<B>, String> {
+		extract_pow_seal::<B>(block).await
+	}
+}
 
 /// Import queue for PoW engine.
 pub fn import_queue<B, C>(
@@ -346,12 +313,11 @@ where
 	C: ProvideRuntimeApi<B> + BlockBackend<B> + Send + Sync + 'static,
 	C::Api: QPoWApi<B>,
 {
-	let verifier = PowVerifier::<B, C>::new();
-
+	let verifier = SimplePowVerifier;
 	Ok(BasicQueue::new(verifier, block_import, justification_import, spawner, registry))
 }
 
-/// Start the mining worker for PoW. This function provides the necessary helper functions that can
+/// Start the mining worker for QPoW. This function provides the necessary helper functions that can
 /// be used to implement a miner. However, it does not do the CPU-intensive mining itself.
 ///
 /// Two values are returned -- a worker, which contains functions that allows querying the current
@@ -519,24 +485,24 @@ where
 	(worker_ret, task)
 }
 
-/// Find PoW pre-runtime.
-fn find_pre_digest<B: BlockT>(header: &B::Header) -> Result<Option<Vec<u8>>, Error<B>> {
-	let mut pre_digest: Option<_> = None;
-	for log in header.digest().logs() {
-		trace!(target: LOG_TARGET, "Checking log {:?}, looking for pre runtime digest", log);
-		match (log, pre_digest.is_some()) {
-			(DigestItem::PreRuntime(POW_ENGINE_ID, _), true) => {
-				return Err(Error::MultiplePreRuntimeDigests)
-			},
-			(DigestItem::PreRuntime(POW_ENGINE_ID, v), false) => {
-				pre_digest = Some(v.clone());
-			},
-			(_, _) => trace!(target: LOG_TARGET, "Ignoring digest not meant for us"),
-		}
-	}
-
-	Ok(pre_digest)
-}
+/// Find QPoW pre-runtime.
+// fn find_pre_digest<B: BlockT>(header: &B::Header) -> Result<Option<Vec<u8>>, Error<B>> {
+// 	let mut pre_digest: Option<_> = None;
+// 	for log in header.digest().logs() {
+// 		trace!(target: LOG_TARGET, "Checking log {:?}, looking for pre runtime digest", log);
+// 		match (log, pre_digest.is_some()) {
+// 			(DigestItem::PreRuntime(POW_ENGINE_ID, _), true) => {
+// 				return Err(Error::MultiplePreRuntimeDigests)
+// 			},
+// 			(DigestItem::PreRuntime(POW_ENGINE_ID, v), false) => {
+// 				pre_digest = Some(v.clone());
+// 			},
+// 			(_, _) => trace!(target: LOG_TARGET, "Ignoring digest not meant for us"),
+// 		}
+// 	}
+//
+// 	Ok(pre_digest)
+// }
 
 /// Fetch PoW seal.
 fn fetch_seal<B: BlockT>(digest: Option<&DigestItem>, hash: B::Hash) -> Result<Vec<u8>, Error<B>> {
@@ -580,7 +546,7 @@ pub fn qpow_verify<B, C>(
 	parent: &BlockId<B>,
 	pre_hash: &H256,
 	seal: &RawSeal,
-) -> Result<(bool, U512), Error<B>>
+) -> Result<bool, Error<B>>
 where
 	B: BlockT<Hash = H256>,
 	C: ProvideRuntimeApi<B>,
@@ -594,7 +560,7 @@ where
 
 	let parent_hash = match extract_block_hash(parent) {
 		Ok(hash) => hash,
-		Err(_) => return Ok((false, U512::zero())),
+		Err(_) => return Ok(false),
 	};
 
 	let pre_hash_arr = pre_hash.as_ref().try_into().unwrap_or([0u8; 32]);
@@ -617,8 +583,8 @@ where
             nonce,
             difficulty
         );
-		return Ok((false, U512::zero()));
+		return Ok(false);
 	}
 
-	Ok((true, difficulty))
+	Ok(true)
 }
