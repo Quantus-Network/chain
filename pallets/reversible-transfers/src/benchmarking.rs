19da2c90fda9a4c8b812c898165e180de68a4c2a
Dastan <88332432+dastansam@users.noreply.github.com>
2025-09-22 11:19:12 +0800

    Fix try-runtime compile

--- pallets/reversible-transfers/src/benchmarking.rs
+++ pallets/reversible-transfers/src/benchmarking.rs
@@ -178,7 +178,6 @@ mod benchmarks {
 		let recipient: T::AccountId = benchmark_account("recipient", 0, SEED);
 		fund_account::<T>(&recipient, BalanceOf::<T>::from(100u128)); // Fund recipient
 		let interceptor: T::AccountId = benchmark_account("interceptor", 1, SEED);
-		let recoverer: T::AccountId = benchmark_account("recoverer", 2, SEED);
 		let transfer_amount = 100u128;
 
 		// Setup owner as reversible and schedule a task in setup
@@ -214,8 +213,8 @@ mod benchmarks {
 		// Check side effect of inner call (balance transfer)
 		let initial_balance = <pallet_balances::Pallet<T> as frame_support::traits::Currency<
 			T::AccountId,
-		>>::minimum_balance() *
-			100_u128.into();
+		>>::minimum_balance()
+			* 100_u128.into();
 		let expected_balance = initial_balance.saturating_add(transfer_amount.into());
 		assert_eq!(
             <pallet_balances::Pallet<T> as frame_support::traits::Currency<T::AccountId>>::free_balance(
