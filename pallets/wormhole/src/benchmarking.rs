cd610e9e9d06912003e270bea2c5f51574eeb43c
Cezary Olborski <cezary.olborski@gmail.com>
2025-10-08 21:46:26 +0800

    fix: Benchmark tests

--- pallets/wormhole/src/benchmarking.rs
+++ pallets/wormhole/src/benchmarking.rs
@@ -1,6 +1,7 @@
 //! Benchmarking setup for pallet-wormhole
 
 use super::*;
+use crate::Pallet as Wormhole;
 use alloc::vec::Vec;
 use frame_benchmarking::v2::*;
 use frame_support::{ensure, traits::fungible::Inspect};
@@ -52,9 +53,21 @@ mod benchmarks {
 
 		let block_number = frame_system::Pallet::<T>::block_number();
 
-		#[extrinsic_call]
-		verify_wormhole_proof(RawOrigin::None, proof_bytes, block_number);
+		#[block]
+		{
+			let _ = Wormhole::<T>::verify_wormhole_proof(
+				RawOrigin::None.into(),
+				proof_bytes,
+				block_number,
+			);
+		}
 
 		Ok(())
+	}
+
+	impl_benchmark_test_suite! {
+		Wormhole,
+		crate::mock::new_test_ext(),
+		crate::mock::Test,
 	}
 }
