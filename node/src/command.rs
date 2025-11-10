6d436cb012fbd9729474f78e7d53b0fc2ff8d101
Cezary Olborski <cezary.olborski@gmail.com>
2025-11-10 11:23:33 +0800

    fix: FMT

--- node/src/command.rs
+++ node/src/command.rs
@@ -179,25 +179,21 @@ impl SubstrateCli for Cli {
 
 	fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
 		Ok(match id {
-			"dev" => {
-				Box::new(chain_spec::development_chain_spec()?) as Box<dyn sc_service::ChainSpec>
-			},
-			"dirac_live_spec" => {
-				Box::new(chain_spec::dirac_chain_spec()?) as Box<dyn sc_service::ChainSpec>
-			},
+			"dev" =>
+				Box::new(chain_spec::development_chain_spec()?) as Box<dyn sc_service::ChainSpec>,
+			"dirac_live_spec" =>
+				Box::new(chain_spec::dirac_chain_spec()?) as Box<dyn sc_service::ChainSpec>,
 			"dirac" => Box::new(chain_spec::ChainSpec::from_json_bytes(include_bytes!(
 				"chain-specs/dirac.json"
 			))?) as Box<dyn sc_service::ChainSpec>,
-			"heisenberg_live_spec" => {
-				Box::new(chain_spec::heisenberg_chain_spec()?) as Box<dyn sc_service::ChainSpec>
-			},
+			"heisenberg_live_spec" =>
+				Box::new(chain_spec::heisenberg_chain_spec()?) as Box<dyn sc_service::ChainSpec>,
 			"" | "heisenberg" => Box::new(chain_spec::ChainSpec::from_json_bytes(include_bytes!(
 				"chain-specs/heisenberg.json"
 			))?) as Box<dyn sc_service::ChainSpec>,
-			path => {
+			path =>
 				Box::new(chain_spec::ChainSpec::from_json_file(std::path::PathBuf::from(path))?)
-					as Box<dyn sc_service::ChainSpec>
-			},
+					as Box<dyn sc_service::ChainSpec>,
 		})
 	}
 }
@@ -413,9 +409,8 @@ pub fn run() -> sc_cli::Result<()> {
 
 						cmd.run(client, inherent_benchmark_data()?, Vec::new(), &ext_factory)
 					},
-					BenchmarkCmd::Machine(cmd) => {
-						cmd.run(&config, SUBSTRATE_REFERENCE_HARDWARE.clone())
-					},
+					BenchmarkCmd::Machine(cmd) =>
+						cmd.run(&config, SUBSTRATE_REFERENCE_HARDWARE.clone()),
 				}
 			})
 		},
