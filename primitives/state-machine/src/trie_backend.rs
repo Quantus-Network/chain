32faaf14d47a823a38eb629d88d413635fe8cf9a
Cezary Olborski <cezary.olborski@gmail.com>
2026-02-27 17:43:52 +0800

    chore: Substrate update stable2506 -> stable2512-2 (#384)
    * draft: PSVM + shallow dependencies
    
    * draft: Working version - without local fully forks updated
    
    * chore: sc-cli - updated
    
    * chore: sc-cli - ForkAware tx pool
    
    * chore: sp-trie & sp-state-machine
    
    * chore: FMT
    
    * chore: Test fixes
    
    * chore: Dependencies + format
    
    * chore: CI new dependencies
    
    * fix: CI - CLANG
    
    * fix: CI - CLANG v2
    
    * fix: CLANG v3
    
    * fix: CI - CLANG v4
    
    * fix: CI - CLANG v5
    
    * fix: CI - CLANG v6
    
    * fix: CI - all full builds update
    
    * fix: Local runtime tests
    
    * fix: PM wormhole check
    
    * chore: Release workflows update
    
    * Update client/cli/src/commands/verify.rs
    
    Co-authored-by: Ethan Cemer <tylercemer@gmail.com>
    
    * fix: crates dependencies
    
    * fix: trie/lib - debug logs
    
    * fix: state-machine and trie - reversed
    
    ---------
    
    Co-authored-by: illuzen <illuzen@users.noreply.github.com>
    Co-authored-by: Ethan Cemer <tylercemer@gmail.com>

--- primitives/state-machine/src/trie_backend.rs
+++ primitives/state-machine/src/trie_backend.rs
@@ -1506,10 +1506,9 @@ pub mod tests {
 		let mut cache = local_cache.as_trie_db_cache(new_root);
 		// All the data should be cached now
 		for (key, value) in new_data {
-			assert_eq!(
-				value.unwrap(),
-				cache.lookup_value_for_key(key).unwrap().data().flatten().unwrap().as_ref()
-			);
+			let cached_data = cache.lookup_value_for_key(key).unwrap().data().flatten().unwrap();
+			let cached: &[u8] = cached_data.as_ref();
+			assert_eq!(value.unwrap(), cached);
 		}
 	}
 
