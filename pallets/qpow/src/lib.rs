1f0192d7297fcd5053a34b2daeddab1cbdb98069
NIK <nheger@gmail.com>
2026-03-14 21:32:47 +0800

    format

--- pallets/qpow/src/lib.rs
+++ pallets/qpow/src/lib.rs
@@ -388,7 +388,7 @@ pub mod pallet {
 		}
 
 		pub fn get_min_difficulty() -> Difficulty {
-			// This value is related to clamp value, 
+			// This value is related to clamp value,
 			// ie, if clamp is 10% this value must be at least 10
 			// 1000 is safe for clamp values >= 0.01%
 			U512::from(1000u64)
